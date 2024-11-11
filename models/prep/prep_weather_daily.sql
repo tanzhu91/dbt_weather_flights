df0444661a474310af50b839c9783d8030835845
Tanzhu Dereli <tanzhu.dereli91@gmail.com>
2024-11-11 12:18:36 +0100

    g

--- models/prep/prep_weather_daily.sql
+++ models/prep/prep_weather_daily.sql
@@ -4,24 +4,24 @@ WITH daily_data AS (
 ),
 add_features AS (
     SELECT *
-		, ... AS date_day
-		, ... AS date_month
-		, ... AS date_year
-		, ... AS cw
-		, ... AS month_name
-		, ... AS weekday
+        , DATE_PART('day', date) AS date_day
+        , DATE_PART('month', date) AS date_month
+        , DATE_PART('year', date) AS date_year
+        , DATE_PART('week', date) AS cw
+        , TO_CHAR(date, 'Month') AS month_name
+        , TO_CHAR(date, 'Day') AS weekday
     FROM daily_data 
 ),
 add_more_features AS (
     SELECT *
-		, (CASE 
-			WHEN month_name in ... THEN 'winter'
-			WHEN ... THEN 'spring'
-            WHEN ... THEN 'summer'
-            WHEN ... THEN 'autumn'
-		END) AS season
+        , (CASE 
+            WHEN DATE_PART('month', date) IN (12, 1, 2) THEN 'winter'
+            WHEN DATE_PART('month', date) IN (3, 4, 5) THEN 'spring'
+            WHEN DATE_PART('month', date) IN (6, 7, 8) THEN 'summer'
+            WHEN DATE_PART('month', date) IN (9, 10, 11) THEN 'autumn'
+        END) AS season
     FROM add_features
 )
 SELECT *
 FROM add_more_features
-ORDER BY date
+ORDER BY date;
