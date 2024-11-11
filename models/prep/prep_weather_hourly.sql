0646d1c422d57732f91373e831cb6058b4fababa
Tanzhu Dereli <tanzhu.dereli91@gmail.com>
2024-11-11 13:42:53 +0100

    g

--- models/prep/prep_weather_hourly.sql
+++ models/prep/prep_weather_hourly.sql
@@ -5,7 +5,7 @@ WITH hourly_data AS (
 add_features AS (
     SELECT *
 		, timestamp::DATE AS date -- only time (hours:minutes:seconds) as TIME data type
-		, timestaamp::TIME AS time -- only time (hours:minutes:seconds) as TIME data type
+		, timestamp::TIME AS time -- only time (hours:minutes:seconds) as TIME data type
         , TO_CHAR(timestamp,'HH24:MI') as hour -- time (hours:minutes) as TEXT data type
         , TO_CHAR(timestamp, 'FMmonth') AS month_name -- month name as a text
         , TO_CHAR(date, 'FMDay') AS weekday -- weekday name as text        
